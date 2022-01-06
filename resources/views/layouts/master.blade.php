88aaa09183aa5c82035d0750705e252831870fc7
maldeve <malandro624@gmail.com>
2022-01-06 14:40:20 +0300

    changed js files path

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -194,12 +194,12 @@
         <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a> 
         <div id="preloader"></div>
 
-        <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
-        <script src="{{ asset('assets/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/scrollreveal/scrollreveal.min.js') }}"></script>
+        <script src="assets/vendor/jquery/jquery.min.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+        <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+        <script src="assets/vendor/scrollreveal/scrollreveal.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
         <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
         <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
